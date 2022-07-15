cb1ec81f8f6ee53279322830fa6f2f5bf1c05998
hana825 <hana825@naver.com>
2022-07-15 13:10:39 +0900

    todolist

--- react-100-todolist/src/components/TodoItem.js
+++ react-100-todolist/src/components/TodoItem.js
@@ -3,11 +3,12 @@ import React from "react";
 function TodoItem(props) {
   const { item, todo_delete, todo_complete } = props;
   return (
-    <tr className={item.b_comp ? "cancel" : ""}>
-      <td onClick={() => todo_complete(item.b_id)}>{item.b_todo}</td>
-      <td className="del" onClick={() => todo_delete(item.b_id)}>
+    <tr className={item.b_comp ? "cancel del" : "del"}>
+      <td className="x" onClick={() => todo_delete(item.b_id)}>
         &times;
       </td>
+      <td onClick={() => todo_complete(item.b_id)}>{item.b_todo}</td>
+      <td className="check">✓</td>
     </tr>
   );
 }
